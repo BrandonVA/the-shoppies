68226ff5de50b0f1e4d2a6c9483814c851ce0e07
BrandonVA <codebva@gmail.com>
2021-05-05 18:29:00 -0700

    Removed alot of logic and passed down functions as props to update state easier on parent component.

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,35 +1,24 @@
-// import { Button } from "bootstrap";
-import React from "react";
-import { Row, Col, Button, Image } from "react-bootstrap";
-import API from "../../utils/API";
+import React, { useEffect, useState } from "react";
+import { Row, Col, Button, Image, Jumbotron } from "react-bootstrap";
 
-const SearchResults = ({ movies }) => {
-  const saveMovie = (movie) => {
-    const movieToSave = {
-      title: movie.Title,
-      year: movie.Year,
-      image: movie.Poster,
-    };
-    API.saveMovie(movieToSave);
-  };
+const SearchResults = ({ movies, saveMovie, savedMoveIDs }) => {
   return (
-    <Row className="m-3 p-3">
+    <Row className="mx-3 px-3">
       {movies.map((movie) => (
         <Col xl={12} key={movie.imdbID}>
           <Row>
             <Col xl={8}>
               <h4>{movie.Title}</h4>
-              <h6>
-                Year Released:
-                {movie.Year}
-              </h6>
             </Col>
             <Col xl={4}>
-              {" "}
               <div style={{ display: "flex", justifyContent: "flex-end" }}>
                 <Button
+                  disabled={savedMoveIDs.includes(movie.imdbID) ? true : false}
                   className="m-1 btn-success"
-                  onClick={() => saveMovie(movie)}
+                  onClick={(e) => {
+                    saveMovie(movie);
+                    e.target.disabled = true;
+                  }}
                 >
                   Save
                 </Button>
@@ -52,7 +41,10 @@ const SearchResults = ({ movies }) => {
                 <div></div>
               )}
             </Col>
-            <Col md={10}></Col>
+            <Col md={10}>
+              {" "}
+              <h6>Year Released: {movie.Year}</h6>
+            </Col>
           </Row>
           <hr />
         </Col>
