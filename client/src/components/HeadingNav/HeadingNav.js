83af6accd0ee89d0c4a2d69fdd80649b6189b117
BrandonVA <codebva@gmail.com>
2021-05-07 20:59:18 -0700

    Cleaned up code.

--- client/src/components/HeadingNav/HeadingNav.js
+++ client/src/components/HeadingNav/HeadingNav.js
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 import "./nav.css";
 
 const HeadingNav = () => {
-  const [location, setLocation] = useState("");
-  useEffect(() => {
-    setLocation(window.location.pathname);
-  }, [location]);
-
-  const setClass = (url) => (url === location ? "active nav-link" : "nav-link");
-
   return (
     <Nav defaultActiveKey="/" as="ul">
-      <Link
-        to="/"
-        onClick={() => {
-          setLocation("/");
-        }}
-        className={"nav-link"}
-      >
+      <Link to="/" className={"nav-link"}>
         The Shoppies
       </Link>
     </Nav>
